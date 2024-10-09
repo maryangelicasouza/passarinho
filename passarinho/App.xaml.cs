eb32a372bac0b1746609a278a164f9f087db94a7
maria <mariaangelicafreitas18@gmail.com>
2024-10-09 09:09:32 -0300

    imagens

---
+++ passarinho/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace passarinho;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ passarinho/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace passarinho;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
