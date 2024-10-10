96d24186c2ae4e0a6bff8cd771783fbcc821cd80
maria <mariaangelicafreitas18@gmail.com>
2024-10-10 10:01:57 -0300

    gravidade

--- passarinho/MainPage.xaml.cs
+++ passarinho/MainPage.xaml.cs
@@ -1,3 +1,27 @@
-﻿
+﻿using Windows.Security.EnterpriseData;
 
+namespace passarinho;
+
+public partial class MainPage : ContentPage
+{
+    
+const int gravidade =1 ;
+const int tempoEntreFrames=25;
+bool estaMorto = false;
+
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+void AplicaGravidade(){
+    passaro.TraslationY += gravidade;
+}
+Protected override void OnAppearing()
+{
+    base.OnAppearing();
+    Desenha();
+}
+}
 
