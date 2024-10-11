312400ac503521c4b77c91030f3f14726802224f
maria <mariaangelicafreitas18@gmail.com>
2024-10-11 08:57:00 -0300

    movimentação canos

--- passarinho/MainPage.xaml.cs
+++ passarinho/MainPage.xaml.cs
@@ -8,6 +8,10 @@ public partial class MainPage : ContentPage
 const int gravidade =1 ;
 const int tempoEntreFrames=25;
 bool estaMorto = false;
+double larguraJanela=0;
+double alturaJanela=0;
+int velocidade= 20;
+
 
 
 	public MainPage()
@@ -15,13 +19,51 @@ bool estaMorto = false;
 		InitializeComponent();
 	}
 
-void AplicaGravidade(){
-    passaro.TraslationY += gravidade;
-}
-Protected override void OnAppearing()
+void AplicaGravidade()
 {
-    base.OnAppearing();
-    Desenha();
+    Passaro.TranslationY += gravidade;
 }
+ public async void Desenha()
+ {
+	while (!estaMorto)
+	{
+		AplicaGravidade();
+		await Task.Delay(tempoEntreFrames);
+		GerenciaCanos ();
+	}
+ }
+
+ void Oi (object s, TappedEventArgs e)
+ {
+	FrameGameOver.IsVisible = false;
+	estaMorto = false;
+	Inicializar();
+	Desenha();
+ }
+
+	void Inicializar()
+	{
+		Passaro.TranslationY = 0;
+	}
+
+	
+		protected override void OnSizeAllocated ( double w, double h)
+		{
+			base.OnSizeAllocated ( w,h);
+			larguraJanela= w;
+			alturaJanela= h ;
+		}
+
+		void GerenciaCanos ()
+		{
+			imgcanocima.TranslationX -= velocidade;
+			imgcanobaixo.TranslationX -= velocidade;
+			if ( imgcanobaixo.TranslationX < -larguraJanela)
+			{
+				imgcanobaixo.TranslationX = 0;
+				imgcanocima.TranslationX =0;
+			}
+		}
+	
 }
 
