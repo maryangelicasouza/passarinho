28d8e19a159be9c9d9ec8a05d089c5808b33659a
Maria <mariaangelicafreitas18@gmail.com>
2024-10-15 08:49:09 -0300

    colisão chao e teto

--- passarinho/MainPage.xaml.cs
+++ passarinho/MainPage.xaml.cs
@@ -4,13 +4,14 @@ namespace passarinho;
 
 public partial class MainPage : ContentPage
 {
-    
-const int gravidade =1 ;
-const int tempoEntreFrames=25;
-bool estaMorto = false;
-double larguraJanela=0;
-double alturaJanela=0;
-int velocidade= 20;
+
+	const int gravidade = 1;
+	const int tempoEntreFrames = 25;
+	bool estaMorto = false;
+	double larguraJanela = 0;
+	double alturaJanela = 0;
+	int velocidade = 20;
+
 
 
 
@@ -19,51 +20,86 @@ int velocidade= 20;
 		InitializeComponent();
 	}
 
-void AplicaGravidade()
-{
-    Passaro.TranslationY += gravidade;
-}
- public async void Desenha()
- {
-	while (!estaMorto)
+	void AplicaGravidade()
 	{
-		AplicaGravidade();
-		await Task.Delay(tempoEntreFrames);
-		GerenciaCanos ();
+		Passaro.TranslationY += gravidade;
 	}
- }
+	public async void Desenha()
+	{
+		while (!estaMorto)
+		{
+			AplicaGravidade();
+			GerenciaCanos();
+			if (VerificaColisao())
+			{
+				estaMorto = true;
+				FrameGameOver.IsVisible = true;
+				break;
+			}
+			await Task.Delay(tempoEntreFrames);
 
- void Oi (object s, TappedEventArgs e)
- {
-	FrameGameOver.IsVisible = false;
-	estaMorto = false;
-	Inicializar();
-	Desenha();
- }
+		}
+	}
+
+	void Oi(object s, TappedEventArgs e)
+	{
+		FrameGameOver.IsVisible = false;
+		estaMorto = false;
+		Inicializar();
+		Desenha();
+	}
 
 	void Inicializar()
 	{
 		Passaro.TranslationY = 0;
 	}
 
-	
-		protected override void OnSizeAllocated ( double w, double h)
-		{
-			base.OnSizeAllocated ( w,h);
-			larguraJanela= w;
-			alturaJanela= h ;
-		}
 
-		void GerenciaCanos ()
+	protected override void OnSizeAllocated(double w, double h)
+	{
+		base.OnSizeAllocated(w, h);
+		larguraJanela = w;
+		alturaJanela = h;
+	}
+
+	void GerenciaCanos()
+	{
+		imgcanocima.TranslationX -= velocidade;
+		imgcanobaixo.TranslationX -= velocidade;
+		if (imgcanobaixo.TranslationX < -larguraJanela)
 		{
-			imgcanocima.TranslationX -= velocidade;
-			imgcanobaixo.TranslationX -= velocidade;
-			if ( imgcanobaixo.TranslationX < -larguraJanela)
+			imgcanobaixo.TranslationX = 4;
+			imgcanocima.TranslationX = 4;
+		}
+	}
+
+	bool VerificaColisao()
+	{
+		if (!estaMorto)
+		{
+			if (VerificaColisaoTeto() ||
+			VerificaColisaoChao())
 			{
-				imgcanobaixo.TranslationX = 0;
-				imgcanocima.TranslationX =0;
+				return true;
 			}
 		}
-	
+		return false;
+	}
+	bool VerificaColisaoTeto()
+	{
+		var minY = -alturaJanela / 2;
+		if (Passaro.TranslationY <= minY)
+			return true;
+		else
+			return false;
+	}
+	bool VerificaColisaoChao()
+	{
+		var maxY = alturaJanela / 2 - chao.HeightRequest;
+		if (Passaro.TranslationY >= maxY)
+			return true;
+		else return false;
+	}
+
 }
 
